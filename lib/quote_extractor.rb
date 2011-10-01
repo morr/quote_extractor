9f2289a66e9b9b9276763ee9511ec2b8ec613608
Andrey Sidorov <takandar@gmail.com>
2011-10-01 12:17:47 +0400

    quotes can be filtered by nesting level from text now

--- lib/quote_extractor.rb
+++ lib/quote_extractor.rb
@@ -1,9 +1,10 @@
-#!/usr/bin/env ruby
+##!/usr/bin/env ruby
 require 'rubygems'
 require 'ap'
 
 #$LOCAL_DEBUG = true
 module QuoteExtractor
+  # вытаскивает из текста информацию о цитатах в нём
   def self.extract(text, pos=0)
     start_pos = pos
     tags = []
@@ -12,20 +13,20 @@ module QuoteExtractor
     while tag_start = text[pos, text.size] =~ /(\[(?:quote(?:=.*?)?|\/quote)\])/
       iteration += 1
       break if iteration > 10
-      ap "extract('#{text}', #{pos}) -> #{text[pos, text.size]}, start: #{tag_start}" if $LOCAL_DEBUG
-      ap "checked: "+text[pos, text.size] if $LOCAL_DEBUG
+      #ap "extract('#{text}', #{pos}) -> #{text[pos, text.size]}, start: #{tag_start}" if $LOCAL_DEBUG
+      #ap "checked: "+text[pos, text.size] if $LOCAL_DEBUG
       tag_size = $1.length
 
       next if $1 =='[/quote]' && pos == 0
       return [tags, pos+tag_start+tag_size] if $1 =='[/quote]'
 
       tag = { :start => pos+tag_start }
-      ap tag if $LOCAL_DEBUG
-      ap tag_size if $LOCAL_DEBUG
-      ap '--->'+text[0, tag[:start]+tag_size] if $LOCAL_DEBUG
+      #ap tag if $LOCAL_DEBUG
+      #ap tag_size if $LOCAL_DEBUG
+      #ap '--->'+text[0, tag[:start]+tag_size] if $LOCAL_DEBUG
       tag[:tags], tag[:end] = extract(text, tag[:start]+tag_size)
-      ap tag if $LOCAL_DEBUG
-      ap '<---'+text[tag[:start], tag[:end]] if $LOCAL_DEBUG
+      #ap tag if $LOCAL_DEBUG
+      #ap '<---'+text[tag[:start], tag[:end]] if $LOCAL_DEBUG
 
       if tag[:end] != -1
         tags << tag
@@ -35,5 +36,21 @@ module QuoteExtractor
       end
     end
     start_pos == 0 ? tags : [tags, -1]
+  end
+
+  # фильтрует из текста все цитаты с уровнем вложенности более допустимого
+  def self.filter(text, nesting, quotes=nil)
+    return filter(text, nesting, extract(text)) unless quotes
+
+    if nesting == 0
+      quotes.reverse.each do |quote|
+        text = text[0, quote[:start]] + text[quote[:end], text.size]
+      end
+    else
+      quotes.reverse.each do |quote|
+        text = filter(text, nesting-1, quote[:tags])
+      end
+    end
+    text
   end
 end
--- spec/lib/quote_extractor_spec.rb
+++ spec/lib/quote_extractor_spec.rb
@@ -1,177 +1,213 @@
 require 'spec_helper'
 
 describe QuoteExtractor do
-  describe 'simple' do
-    describe 'one' do
-      it "[quote][/quote]" do
-        QuoteExtractor.extract("[quote][/quote]").should == [{
-            :tags => [],
-            :start => 0,
-            :end => 15
-        }]
+  describe 'extracts' do
+    describe 'simple' do
+      describe 'one' do
+        it "[quote][/quote]" do
+          QuoteExtractor.extract("[quote][/quote]").should == [{
+              :tags => [],
+              :start => 0,
+              :end => 15
+          }]
+        end
+
+        it "[quote=morr][/quote]" do
+          QuoteExtractor.extract("[quote=morr][/quote]").should == [{
+              :tags => [],
+              :start => 0,
+              :end => 20
+          }]
+        end
+
+        it "[quote][/quote]a" do
+          QuoteExtractor.extract("[quote][/quote]a").should == [{
+              :tags => [],
+              :start => 0,
+              :end => 15
+          }]
+        end
+
+        it "[quote]d[/quote]" do
+          QuoteExtractor.extract("[quote]d[/quote]").should == [{
+              :tags => [],
+              :start => 0,
+              :end => 16
+          }]
+        end
+
+        it "[quote]d[/quote]e" do
+          QuoteExtractor.extract("[quote]d[/quote]e").should == [{
+              :tags => [],
+              :start => 0,
+              :end => 16
+          }]
+        end
       end
 
-      it "[quote=morr][/quote]" do
-        QuoteExtractor.extract("[quote=morr][/quote]").should == [{
-            :tags => [],
-            :start => 0,
-            :end => 20
-        }]
-      end
+      describe 'multiple' do
+        it "[quote][/quote][quote][/quote]" do
+          QuoteExtractor.extract("[quote][/quote][quote][/quote]").should == [{
+              :tags => [],
+              :start => 0,
+              :end => 15
+          }, {
+              :tags => [],
+              :start => 15,
+              :end => 30
+          }]
+        end
 
-      it "[quote][/quote]a" do
-        QuoteExtractor.extract("[quote][/quote]a").should == [{
-            :tags => [],
-            :start => 0,
-            :end => 15
-        }]
-      end
+        it "aa[quote]zxc[/quote]bb[quote][/quote]zz" do
+          QuoteExtractor.extract("aa[quote]zxc[/quote]bb[quote][/quote]zz").should == [{
+              :tags => [],
+              :start => 2,
+              :end => 20
+          }, {
+              :tags => [],
+              :start => 22,
+              :end => 37
+          }]
+        end
 
-      it "[quote]d[/quote]" do
-        QuoteExtractor.extract("[quote]d[/quote]").should == [{
-            :tags => [],
-            :start => 0,
-            :end => 16
-        }]
-      end
-
-      it "[quote]d[/quote]e" do
-        QuoteExtractor.extract("[quote]d[/quote]e").should == [{
-            :tags => [],
-            :start => 0,
-            :end => 16
-        }]
+        it "[quote][/quote][quote][/quote][quote][/quote]" do
+          QuoteExtractor.extract("[quote][/quote][quote][/quote][quote][/quote]").should == [{
+              :tags => [],
+              :start => 0,
+              :end => 15
+          }, {
+              :tags => [],
+              :start => 15,
+              :end => 30
+          }, {
+              :tags => [],
+              :start => 30,
+              :end => 45
+          }]
+        end
       end
     end
 
-    describe 'multiple' do
-      it "[quote][/quote][quote][/quote]" do
-        QuoteExtractor.extract("[quote][/quote][quote][/quote]").should == [{
-            :tags => [],
+    describe 'nested' do
+      it "[quote][quote][/quote][/quote]" do
+        QuoteExtractor.extract("[quote][quote][/quote][/quote]").should == [{
+            :tags => [{
+              :tags => [],
+              :start => 7,
+              :end => 22
+            }],
             :start => 0,
-            :end => 15
-        }, {
-            :tags => [],
-            :start => 15,
             :end => 30
         }]
       end
 
-      it "aa[quote]zxc[/quote]bb[quote][/quote]zz" do
-        QuoteExtractor.extract("aa[quote]zxc[/quote]bb[quote][/quote]zz").should == [{
-            :tags => [],
-            :start => 2,
-            :end => 20
-        }, {
-            :tags => [],
-            :start => 22,
-            :end => 37
+      it "z[quote]1[quote]zxcv[/quote][/quote]f" do
+        QuoteExtractor.extract("z[quote]1[quote]zxcv[/quote][/quote]f").should == [{
+            :tags => [{
+              :tags => [],
+              :start => 9,
+              :end => 28
+            }],
+            :start => 1,
+            :end => 36
         }]
       end
 
-      it "[quote][/quote][quote][/quote][quote][/quote]" do
-        QuoteExtractor.extract("[quote][/quote][quote][/quote][quote][/quote]").should == [{
-            :tags => [],
+      it "[quote][quote][/quote][quote][/quote][/quote]" do
+        QuoteExtractor.extract("[quote][quote][/quote][quote][/quote][/quote]").should == [{
+            :tags => [{
+              :tags => [],
+              :start => 7,
+              :end => 22
+            }, {
+              :tags => [],
+              :start => 22,
+              :end => 37
+            }],
             :start => 0,
-            :end => 15
-        }, {
-            :tags => [],
-            :start => 15,
-            :end => 30
-        }, {
-            :tags => [],
-            :start => 30,
             :end => 45
         }]
       end
-    end
-  end
 
-  describe 'nested' do
-    it "[quote][quote][/quote][/quote]" do
-      QuoteExtractor.extract("[quote][quote][/quote][/quote]").should == [{
-          :tags => [{
-            :tags => [],
-            :start => 7,
-            :end => 22
-          }],
-          :start => 0,
-          :end => 30
-      }]
-    end
-
-    it "z[quote]1[quote]zxcv[/quote][/quote]f" do
-      QuoteExtractor.extract("z[quote]1[quote]zxcv[/quote][/quote]f").should == [{
-          :tags => [{
-            :tags => [],
-            :start => 9,
-            :end => 28
-          }],
-          :start => 1,
-          :end => 36
-      }]
-    end
-
-    it "[quote][quote][/quote][quote][/quote][/quote]" do
-      QuoteExtractor.extract("[quote][quote][/quote][quote][/quote][/quote]").should == [{
-          :tags => [{
-            :tags => [],
-            :start => 7,
-            :end => 22
-          }, {
-            :tags => [],
-            :start => 22,
-            :end => 37
-          }],
-          :start => 0,
-          :end => 45
-      }]
-    end
-
-    it "[quote][quote][/quote][quote][quote]yahoo![/quote][/quote][/quote]" do
-      QuoteExtractor.extract("[quote][quote][/quote][quote][quote]yahoo![/quote][/quote][/quote]").should == [{
-          :tags => [{
-            :tags => [],
-            :start => 7,
-            :end => 22
-          }, {
+      it "[quote][quote][/quote][quote][quote]yahoo![/quote][/quote][/quote]" do
+        QuoteExtractor.extract("[quote][quote][/quote][quote][quote]yahoo![/quote][/quote][/quote]").should == [{
             :tags => [{
               :tags => [],
-              :start => 29,
-              :end => 50
+              :start => 7,
+              :end => 22
+            }, {
+              :tags => [{
+                :tags => [],
+                :start => 29,
+                :end => 50
+              }],
+              :start => 22,
+              :end => 58
             }],
-            :start => 22,
-            :end => 58
-          }],
-          :start => 0,
-          :end => 66
-      }]
+            :start => 0,
+            :end => 66
+        }]
+      end
+    end
+
+    describe 'broken' do
+      it "[quote]" do
+        QuoteExtractor.extract("[quote]").should == []
+      end
+
+      it "[/quote]" do
+        QuoteExtractor.extract("[/quote]").should == []
+      end
+
+      it "[/quote][quote]" do
+        QuoteExtractor.extract("[/quote][quote]").should == []
+      end
+
+      it "[quote][quote]" do
+        QuoteExtractor.extract("[quote][quote]").should == []
+      end
+
+      it "[quote][quote][/quote]" do
+        QuoteExtractor.extract("[quote][quote][/quote]").should == [{
+          :tags => [],
+          :start => 7,
+          :end => 22,
+        }]
+      end
     end
   end
 
-  describe 'broken' do
-    it "[quote]" do
-      QuoteExtractor.extract("[quote]").should == []
+  describe 'filters' do
+    describe '0 lvl' do
+      it 'simple' do
+        QuoteExtractor.filter('a[quote]b[/quote]c', 0).should == 'ac'
+        QuoteExtractor.filter('a[quote][quote]b[/quote][/quote]c', 0).should == 'ac'
+        QuoteExtractor.filter('[quote]b[/quote]c', 0).should == 'c'
+        QuoteExtractor.filter('[quote][/quote]c', 0).should == 'c'
+        QuoteExtractor.filter('[quote][/quote]', 0).should == ''
+        QuoteExtractor.filter('[quote=morr][/quote]', 0).should == ''
+      end
+
+      it 'multiple' do
+        QuoteExtractor.filter('a[quote]b[/quote]c[quote]d[/quote]e', 0).should == 'ace'
+      end
     end
 
-    it "[/quote]" do
-      QuoteExtractor.extract("[/quote]").should == []
+    describe '1 lvl' do
+      it 'simple' do
+        QuoteExtractor.filter('a[quote]b[quote]c[/quote]d[/quote]e', 1).should == 'a[quote]bd[/quote]e'
+        QuoteExtractor.filter('a[quote]b[quote]c[quote]d[/quote]e[/quote]f[/quote]g', 1).should == 'a[quote]bf[/quote]g'
+      end
+
+      it 'multiple' do
+        QuoteExtractor.filter('a[quote]b[quote]c[/quote]d[quote]e[/quote]f[/quote]g', 1).should == 'a[quote]bdf[/quote]g'
+      end
     end
 
-    it "[/quote][quote]" do
-      QuoteExtractor.extract("[/quote][quote]").should == []
-    end
-
-    it "[quote][quote]" do
-      QuoteExtractor.extract("[quote][quote]").should == []
-    end
-
-    it "[quote][quote][/quote]" do
-      QuoteExtractor.extract("[quote][quote][/quote]").should == [{
-        :tags => [],
-        :start => 7,
-        :end => 22,
-      }]
+    describe '2 lvl' do
+      it 'simple' do
+        QuoteExtractor.filter('a[quote]b[quote]c[quote]d[/quote]e[/quote]f[/quote]g', 2).should == 'a[quote]b[quote]ce[/quote]f[/quote]g'
+      end
     end
   end
 end
